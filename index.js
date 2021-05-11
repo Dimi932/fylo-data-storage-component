dd044e71a280097919c036d7b44c686b1035563d
Dimi932 <77401923+Dimi932@users.noreply.github.com>
2021-05-11 06:12:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-// var bold = document.querySelector(".bold");
-// var strong = document.querySelector("strong");
-// var left = document.querySelector(".left");
-// var right = document.querySelector(".right");
+
 var slider = document.querySelector("#myinput");
-var f = document.querySelector("#myinput").min;
-var g = document.querySelector("#myinput").max;
 var strong = document.querySelector("strong");
 var bold = document.querySelector(".bold");
 strong.innerHTML = slider.value
@@ -12,8 +7,6 @@ strong.innerHTML = slider.value
 
 slider.oninput = function() {
 strong.innerHTML = this.value;
-bold.innerHTML = this.value;
+bold.innerHTML = 1000 - this.value;
 
 }
-// bold.innerHTML = this.value;
-// strong.innerHTML = this.value;
